9063231e36c2c1ea9726f3e44d959517a3b61f95
Atanas Chebishev <114015858+chebishev@users.noreply.github.com>
2023-02-01 15:37:04 +0200

    Update yourube_link_trimmer.py

--- yourube_link_trimmer.py
+++ yourube_link_trimmer.py
@@ -3,14 +3,23 @@ print("Hello, I am very useful program. ")
 print("I can remove \"music.\" and '&feature=share' from YouTube music links. ")
 print("Find a link and follow the next prompt!")
 
+function_dictionary = {
+  "remove_music": lambda x: x.replace("music.", ""),
+  "trim_link": lambda x: x.replace("&feature=share", "")
+}
+
 # Taking user input
 original_youtube_link = input("Paste your YouTube Music link here: ")
 
 # This string replace function removes "music." from the YouTube link
-remove_music = original_youtube_link.replace("music.", "")
+remove_music = function_dictionary["remove_music"](original_youtube_link)
+# it does the same thing as:
+# remove_music = original_youtube_link.replace("music.", "")
 
 # This function removes the final part of the link: "&feature=share"
-trimmed_link = remove_music.replace("&feature=share", "")
+trimmed_link = function_dictionary["trim_link"](original_youtube_link)
+# same functionality as:
+# trimmed_link = remove_music.replace("&feature=share", "")
 
 # printing the trimmed link
 print("Here is your trimmed link:")
